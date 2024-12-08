7be78dbb3ae3cc24e15bdf9e4f7a80fd16f24d50
akram-abid <a.abid44778@gmail.com>
2024-12-08 14:53:20 +0100

    feat: make the code more readable, add remove book function, add change book status function

--- javascript.js
+++ javascript.js
@@ -1,70 +1,75 @@
 const myLibrary = [];
-let index = 0;
 
 function Crbook(name, author, date, read) {
   this.name = name;
   this.author = author;
   this.date = date;
   this.read = read;
-  this.sayMe = function () {
-    console.log(`title:${this.name} author:${this.author}`);
-  };
 }
-
-const book1 = new Crbook("little", "me", "now", false);
-book1.name = "huge";
-console.log(book1.name);
-book1.sayMe();
-myLibrary.push(book1);
-myLibrary[0].sayMe();
 
 const book_name = document.querySelector("#book-name");
 const book_author = document.querySelector("#book-author");
 const book_date = document.querySelector("#book-release-date");
 const book_read = document.querySelector("#have-read");
-console.log(book_read.checked);
 const books = document.querySelector(".books");
-
 const add = document.querySelector("#btn");
-add.addEventListener("click", () => {
-  
-  const book = document.createElement("div");
-  book.classList.add("book");
-  const bookName = document.createElement("p");
-  bookName.classList.add("name");
-  const author = document.createElement("p");
-  author.classList.add("author");
-  const date = document.createElement("p");
-  date.classList.add("date");
-  const label = document.createElement("p");
-  const input = document.createElement("input");
-  input.type = "checkbox";
-  input.name = "read";
-  input.disabled = true;
-  const isread = document.createElement("div");
-  const newBook = new Crbook(
-    `Book Name: ${book_name.textContent}`,
-    `Author: ${book_author}`,
-    `date release: ${book_date}`,
-    `book status: ${book_read}`
-  );
-  myLibrary.push(newBook);
-  myLibrary.forEach((bookElement) => {
-    bookName.textContent = book_name.value;
-    author.textContent = book_author.value;
-    date.textContent = book_date.value;
-    input.checked = book_read.checked;
-    isread.classList.add("is-read");
-    isread.appendChild(label);
-    isread.appendChild(input);
-    book.dataset.index = index;
-    index += 1;
+
+function renderBooks() {
+  books.innerHTML = ""; 
+  myLibrary.forEach((bookElement, index) => {
+    const book = document.createElement("div");
+    book.classList.add("book");
+
+    const bookName = document.createElement("p");
+    bookName.textContent = `Name: ${bookElement.name}`;
+
+    const author = document.createElement("p");
+    author.textContent = `Author: ${bookElement.author}`;
+
+    const date = document.createElement("p");
+    date.textContent = `Release Date: ${bookElement.date}`;
+
+    const input = document.createElement("input");
+    input.type = "checkbox";
+    input.disabled = true; 
+    input.checked = bookElement.read;
+
+    const toggleRead = document.createElement("button");
+    toggleRead.textContent = bookElement.read ? "Mark as Unread" : "Mark as Read";
+    toggleRead.dataset.index = index;
+    toggleRead.addEventListener("click", (e) => {
+      const index = e.target.dataset.index;
+      myLibrary[index].read = !myLibrary[index].read; 
+      renderBooks(); 
+    });
+
+    const remove = document.createElement("button");
+    remove.textContent = "Remove";
+    remove.dataset.index = index;
+    remove.addEventListener("click", (e) => {
+      const index = e.target.dataset.index;
+      myLibrary.splice(index, 1);
+      renderBooks();
+    });
+
     book.appendChild(bookName);
     book.appendChild(author);
     book.appendChild(date);
-    book.appendChild(isread);
+    book.appendChild(input);
+    book.appendChild(toggleRead);
+    book.appendChild(remove);
+
     books.appendChild(book);
   });
-  console.log(book_name.value);
-  console.log("clicked");
+}
+
+add.addEventListener("click", () => {
+  const newBook = new Crbook(
+    book_name.value,
+    book_author.value,
+    book_date.value,
+    book_read.checked
+  );
+  myLibrary.push(newBook);
+  renderBooks();
 });
